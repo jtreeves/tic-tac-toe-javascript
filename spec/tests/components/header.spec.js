a8fa9efc13fc6b60d91b81427a38023d39a48744
jtreeves <jackson.t.reeves@gmail.com>
2022-10-20 00:33:04 -0400

    [spec] Add jasmine for testing

---
+++ spec/tests/components/header.spec.js
@@ -0,0 +1,8 @@
+import { header } from '../../../components/header.js'
+
+describe('header component', () => {
+    it('should create a node', () => {
+        // const h1 = header()
+        expect(true).toBeTruthy()
+    })
+})
