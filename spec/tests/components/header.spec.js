0ad2fd18c3136e84280accff642c439b67e5c9f0
jtreeves <jackson.t.reeves@gmail.com>
2022-10-21 22:05:58 -0400

    Delete Node files

--- spec/tests/components/header.spec.js
+++
@@ -1,8 +0,0 @@
-import { header } from '../../../components/header.js'
-
-describe('header component', () => {
-    it('should create a node', () => {
-        // const h1 = header()
-        expect(true).toBeTruthy()
-    })
-})
