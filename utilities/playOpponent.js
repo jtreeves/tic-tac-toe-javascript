511e6a40eabb5e5a42470370443314802c2a7cab
jtreeves <jackson.t.reeves@gmail.com>
2022-10-21 20:26:12 -0400

    [playOpponent] Use helper functions to automate computer's turn

---
+++ utilities/playOpponent.js
@@ -0,0 +1,25 @@
+import { getPlayer } from '../accessors/getPlayer.js'
+import { selectBestCell } from './selectBestCell.js'
+import { getPoints } from '../accessors/getPoints.js'
+import { setPoints } from '../storers/setPoints.js'
+import { checkIfTie } from './checkIfTie.js'
+import { checkIfWinner } from './checkIfWinner.js'
+import { updateTurn } from '../mutators/updateTurn.js'
+import { updateMessage } from '../mutators/updateMessage.js'
+
+export function playOpponent() {
+    const player = getPlayer()
+    const opponent = player * -1
+    const computer = opponent === 1 ? 'X' : 'O'
+    const cell = selectBestCell(computer)
+    cell.textContent = computer
+    const id = cell.id
+    const index = extractIndexFromId(id)
+    const points = getPoints()
+    points[index] = opponent
+    setPoints(points)
+    const tie = checkIfTie()
+    const winner = checkIfWinner()
+    updateTurn(winner)
+    updateMessage(winner, tie)
+}
